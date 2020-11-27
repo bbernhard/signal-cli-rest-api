48448cdca1da1e9f11b3c0333aecaf414f94c141
FL42 <46161216+fl42@users.noreply.github.com>
2020-11-27 22:29:59 +0100

    feat: show warning when exec in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,11 @@ set -e
 # Fix permissions to ensure backward compatibility
 chown 1000:1000 -R /home/.local/share/signal-cli
 
-# Start API
+# Show warning on docker exec
+cat <<EOF >> /root/.bashrc
+echo "WARNING: signal-cli-rest-api runs as signal-api (not as root!)" 
+echo "Run 'su signal-api' before using signal-cli!"
+EOF
+
+# Start API as signal-api user
 exec su -s /bin/sh -c "exec signal-cli-rest-api" signal-api
