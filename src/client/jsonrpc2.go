444e7d7608ecef6121993f72fd2a9d33048fcf67
Bernhard B <schluchti@gmail.com>
2021-09-14 18:21:06 +0200

    replaced info log with debug log

--- src/client/jsonrpc2.go
+++ src/client/jsonrpc2.go
@@ -72,7 +72,7 @@ func (r *JsonRpc2Client) getRaw(command string, args interface{}) (string, error
 		return "", err
 	}
 
-	log.Info("request = ", string(fullCommandBytes))
+	log.Debug("full command ", string(fullCommandBytes))
 
 	_, err = r.conn.Write([]byte(string(fullCommandBytes) + "\n"))
 	if err != nil {
