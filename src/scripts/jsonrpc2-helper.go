bb8c0efcacf99a57b133df5faf34c54f1326b0a5
Bernhard B <bernhard@liftingcoder.com>
2026-03-07 21:08:18 +0100

    changes signal-json-rpc run script
    * switched to signal-cli daemon mode - this way we don't need to
      pipe signal-cli's output to netcat.
    
    * removed fifo pathname from the json-rpc config (not needed anymore)

--- src/scripts/jsonrpc2-helper.go
+++ src/scripts/jsonrpc2-helper.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
-	"os/exec"
+	"strconv"
 	"strings"
+	"syscall"
 
 	"github.com/bbernhard/signal-cli-rest-api/utils"
 	log "github.com/sirupsen/logrus"
 )
-
-const supervisorctlConfigTemplate = `
-[program:%s]
-process_name=%s
-command=signal-cli --output=json --config %s%s daemon %s%s --tcp 127.0.0.1:%d
-autostart=true
-autorestart=true
-startretries=10
-user=signal-api
-directory=/usr/bin/
-redirect_stderr=true
-stdout_logfile=/var/log/%s/out.log
-stderr_logfile=/var/log/%s/err.log
-stdout_logfile_maxbytes=50MB
-stdout_logfile_backups=10
-numprocs=1
-`
 
 func main() {
 	signalCliConfigDir := "/home/.local/share/signal-cli/"
@@ -41,72 +23,47 @@ func main() {
 	jsonRpc2ClientConfig := utils.NewJsonRpc2ClientConfig()
 
 	var tcpPort int64 = 6001
-	fifoPathname := "/tmp/sigsocket1"
+	jsonRpc2ClientConfig.AddEntry(utils.MULTI_ACCOUNT_NUMBER, utils.JsonRpc2ClientConfigEntry{TcpPort: tcpPort})
 
-	jsonRpc2ClientConfig.AddEntry(utils.MULTI_ACCOUNT_NUMBER, utils.JsonRpc2ClientConfigEntry{TcpPort: tcpPort, FifoPathname: fifoPathname})
+	args := []string{"--output=json", "--config", signalCliConfigDir}
 
-	os.Remove(fifoPathname) //remove any existing named pipe
-
-	_, err := exec.Command("mkfifo", fifoPathname).Output()
-	if err != nil {
-		log.Fatal("Couldn't create fifo with name ", fifoPathname, ": ", err.Error())
-	}
-
-	uid := utils.GetEnv("SIGNAL_CLI_UID", "1000")
-	gid := utils.GetEnv("SIGNAL_CLI_GID", "1000")
-	_, err = exec.Command("chown", uid+":"+gid, fifoPathname).Output()
-	if err != nil {
-		log.Fatal("Couldn't change permissions of fifo with name ", fifoPathname, ": ", err.Error())
-	}
-
-	signalCliIgnoreAttachments := ""
-	ignoreAttachments := utils.GetEnv("JSON_RPC_IGNORE_ATTACHMENTS", "")
-	if ignoreAttachments == "true" {
-		signalCliIgnoreAttachments = " --ignore-attachments"
-	}
-
-	signalCliIgnoreStories := ""
-	ignoreStories := utils.GetEnv("JSON_RPC_IGNORE_STORIES", "")
-	if ignoreStories == "true" {
-		signalCliIgnoreStories = " --ignore-stories"
-	}
-
-	supervisorctlProgramName := "signal-cli-json-rpc-1"
-	supervisorctlLogFolder := "/var/log/" + supervisorctlProgramName
-	_, err = exec.Command("mkdir", "-p", supervisorctlLogFolder).Output()
-	if err != nil {
-		log.Fatal("Couldn't create log folder ", supervisorctlLogFolder, ": ", err.Error())
-	}
-
-	trustNewIdentities := ""
 	trustNewIdentitiesEnv := utils.GetEnv("JSON_RPC_TRUST_NEW_IDENTITIES", "")
 	if trustNewIdentitiesEnv == "on-first-use" {
-		trustNewIdentities = " --trust-new-identities on-first-use"
+		args = append(args, []string{"--trust-new-identities", "on-first-use"}...)
 	} else if trustNewIdentitiesEnv == "always" {
-		trustNewIdentities = " --trust-new-identities always"
+		args = append(args, []string{"--trust-new-identities", "always"}...)
 	} else if trustNewIdentitiesEnv == "never" {
-		trustNewIdentities = " --trust-new-identities never"
+		args = append(args, []string{"--trust-new-identities", "never"}...)
 	} else if trustNewIdentitiesEnv != "" {
 		log.Fatal("Invalid JSON_RPC_TRUST_NEW_IDENTITIES environment variable set!")
 	}
 
-	log.Info("Updated jsonrpc2.yml")
+	args = append(args, "daemon")
 
-	//write supervisorctl config
-	supervisorctlConfigFilename := "/etc/supervisor/conf.d/" + "signal-cli-json-rpc-1.conf"
-
-	supervisorctlConfig := fmt.Sprintf(supervisorctlConfigTemplate, supervisorctlProgramName, supervisorctlProgramName,
-		signalCliConfigDir, trustNewIdentities, signalCliIgnoreAttachments, signalCliIgnoreStories, tcpPort,
-		supervisorctlProgramName, supervisorctlProgramName)
-
-	err = ioutil.WriteFile(supervisorctlConfigFilename, []byte(supervisorctlConfig), 0644)
-	if err != nil {
-		log.Fatal("Couldn't write ", supervisorctlConfigFilename, ": ", err.Error())
+	ignoreAttachments := utils.GetEnv("JSON_RPC_IGNORE_ATTACHMENTS", "")
+	if ignoreAttachments == "true" {
+		args = append(args, " --ignore-attachments")
 	}
 
+	ignoreStories := utils.GetEnv("JSON_RPC_IGNORE_STORIES", "")
+	if ignoreStories == "true" {
+		args = append(args, " --ignore-stories")
+	}
+
+	args = append(args, []string{"--tcp", "127.0.0.1:" + strconv.FormatInt(tcpPort, 10)}...)
+
 	// write jsonrpc.yml config file
-	err = jsonRpc2ClientConfig.Persist(signalCliConfigDir + "jsonrpc2.yml")
+	err := jsonRpc2ClientConfig.Persist(signalCliConfigDir + "jsonrpc2.yml")
 	if err != nil {
 		log.Fatal("Couldn't persist jsonrpc2.yaml: ", err.Error())
+	}
+
+	log.Info("Updated jsonrpc2.yml")
+
+	env := os.Environ()
+
+	err = syscall.Exec("/usr/bin/signal-cli", args, env)
+	if err != nil {
+		log.Fatal("Couldn't start signal-cli in json-rpc mode: ", err.Error())
 	}
 }
